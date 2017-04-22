3fedcf7954422cc9e0f86db4852898ac4d33d44e
jswizzy <smithjustinn@hotmail.com>
2017-04-22 17:55:21 -0400

    work on handshake

---
+++ elixir/secret-handshake/secret_handshake.exs
@@ -0,0 +1,38 @@
+defmodule SecretHandshake do
+  @secret_code {"wink", "double blink", "close your eyes", "jump"}
+  @doc """
+  Determine the actions of a secret handshake based on the binary
+  representation of the given `code`.
+
+  If the following bits are set, include the corresponding action in your list
+  of commands, in order from lowest to highest.
+
+  1 = wink
+  10 = double blink
+  100 = close your eyes
+  1000 = jump
+
+  10000 = Reverse the order of the operations in the secret handshake
+  """
+  @spec commands(code :: integer) :: list(String.t())
+  def commands(code) do
+    code
+    |> Integer.digits(2)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> handshake()
+    |> IO.inspect
+  end
+
+  defp handshake({code, index} , code \\ [] ) do
+    
+  end
+
+end
+
+
+
+SecretHandshake.commands(1)
+SecretHandshake.commands(2)
+SecretHandshake.commands(4)
+SecretHandshake.commands(15)
