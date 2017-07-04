407afd712bcf3148764f70de3906cfbc60edb179
jswizzy <smithjustinn@hotmail.com>
2017-07-03 21:03:24 -0400

    add accumulate without side effects

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -3,8 +3,9 @@ package accumulate
 const testVersion = 1
 
 func Accumulate(stringSlice []string, fun func(string) string) []string {
+	newSlice := make([]string, len(stringSlice))
 	for i, str := range stringSlice {
-		stringSlice[i] = fun(str)
+		newSlice[i] = fun(str)
 	}
-	return stringSlice
+	return newSlice
 }
